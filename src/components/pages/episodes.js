21f36b0ef6b9af370a9add593db869c52442b95d
AarshShah9 <aarshshah99@gmail.com>
2022-12-25 13:57:10 -0600

    Added Paragraphs to homepage

---
+++ src/components/pages/episodes.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Episodes() {
+  return <div>Episodes</div>;
+}
+
+export default Episodes;
---
+++ src/components/pages/home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <p>
+        Suffering in Paradise is a podcast that aims to provide a unique
+        perspective of the social dynamics of growing up in a westernized
+        paradise by leveraging guests’ experiences, knowledge, and wisdom.
+      </p>
+      <h4>Mission and Vision</h4>
+      <p>
+        The vision is to provide an opportunity for our audience to enrich their
+        understanding of the world they live in by connecting with modern-day
+        leaders and changemakers that utilize perspective as a driver in their
+        daily lives.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
