d72deeaa6a41671833a459b973a1c9df86e284d6
AarshShah9 <aarshshah99@gmail.com>
2022-12-30 15:16:52 -0600

    added credentials for people to add custom values

---
+++ src/components/credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+  return {
+    ClientId: "Add Client ID here",
+    ClientSecret: "Add Client Secret here",
+    showId: "Add show ID here",
+  };
+};
+
+export { Credentials };
