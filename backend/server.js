a90cb14856541ccbd6812aaad429a441d97b9e60
AarshShah9 <aarshshah99@gmail.com>
2023-02-08 17:37:27 -0700

    got rid of node_modules

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,38 @@
 const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-
-require("dotenv").config();
-
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
+app.get("/", (req, res) => {
+  res.json({ mssg: "Hello World" });
 });
 
-// const exercisesRouter = require("./routes/exercises");
-// const usersRouter = require("./routes/users");
-
-// app.use("/users", usersRouter);
-
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+// Listen for requests
+app.listen(4000, () => {
+  console.log("Now listening for requests!");
 });
+
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+
+// require("dotenv").config();
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(cors());
+// app.use(express.json());
+
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("MongoDB database connection established successfully");
+// });
+
+// // const exercisesRouter = require("./routes/exercises");
+// // const usersRouter = require("./routes/users");
+
+// // app.use("/users", usersRouter);
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
